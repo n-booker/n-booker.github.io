f0bd7151bfa0f82028173ef34d5c381ce63c4159
n-booker <93396647+n-booker@users.noreply.github.com>
2025-01-09 05:34:53 +0000

    Add files via upload

--- components/header.js
+++ components/header.js
@@ -5,11 +5,11 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <div id="main">
-        <a href="pdffiles/Metrics and Cosmos and Trilobites.pdf">GR & Cosmology</a> - 
-        <a href="pdffiles/Particles & Wizards.pdf">QFT (in progress)</a> - 
-        <a href="pdffiles/Electron's Destiny.pdf">Advanced QM</a> - 
-        <a href="pdffiles/Spinors & Symmetries.pdf">Symmetries (in progress)</a>
+      <div id="main" style="text-align: center;">
+        <a href="pdffiles/Metrics and Cosmos and Trilobites.pdf" target="_blank">GR & Cosmology</a> - 
+        <a href="pdffiles/Particles & Wizards.pdf" target="_blank">QFT (in progress)</a> - 
+        <a href="pdffiles/Electron's Destiny.pdf" target="_blank">Advanced QM</a> - 
+        <a href="pdffiles/Spinors & Symmetries.pdf" target="_blank">Symmetries (in progress)</a>
         <hr>
       </div>
     `;
