97a2e889b7c81cf161c1504f165f29e88cafe8d2
n-booker <93396647+n-booker@users.noreply.github.com>
2025-02-06 23:10:24 +0000

    Update header.js

--- components/header.js
+++ components/header.js
@@ -10,17 +10,12 @@ class Header extends HTMLElement {
         <b>
         <a href="index.html" class="special-link">Main Page</a> - 
         <a href="physics.html" class="special-link">Physics</a> - 
-        <a href="latex.html" class="special-link">\(\mathrm{\LaTeX\})</a> - 
+        <a href="latex.html" class="special-link">LaTeX</a> - 
         <a href="3D.html" class="special-link">3D Modelling</a>
         <b>
         <hr>
       </div>
     `;
-    
-    // Re-render MathJax after content injection
-    if (window.MathJax) {
-      window.MathJax.typesetPromise([this]);
-    }
   }
 }
 
