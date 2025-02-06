5ac020854198ed04c5cc84a04270b6d60fdf60fc
n-booker <93396647+n-booker@users.noreply.github.com>
2025-02-06 22:42:37 +0000

    Add files via upload

--- components/header.js
+++ components/header.js
@@ -8,9 +8,10 @@ class Header extends HTMLElement {
       <div id="header" class="special-link">
         <hr>
         <b>
-        <a href="index.html" class="special-link">Home</a> - 
-        <a href="notes.html" class="special-link">Lecture Notes</a> - 
-        <a href="3D.html" class="special-link">3D Renders</a>
+        <a href="index.html" class="special-link">Main Page</a> - 
+        <a href="physics.html" class="special-link">Physics</a> - 
+        <a href="latex.html" class="special-link">\(\LaTeX\)</a> - 
+        <a href="3D.html" class="special-link">3D Modelling</a>
         <b>
         <hr>
       </div>
