277aea355e306fe7abaafc78eb12299c9ec2ffe5
n-booker <93396647+n-booker@users.noreply.github.com>
2025-04-23 14:26:08 +0800

    Update header.js

--- components/header.js
+++ components/header.js
@@ -12,7 +12,7 @@ class Header extends HTMLElement {
         <a href="research.html" class="special-link">Research</a> - 
         <a href="courses.html" class="special-link">List of Courses</a> - 
         <a href="latex.html" class="special-link">LaTeX</a> - 
-        <a href="3D.html" class="special-link">3D Modelling</a>
+        <a href="3D.html" class="special-link">3D Modelling</a> - 
         <a href="ijcps.html" class="special-link">Journal Club</a>
         <b>
         <hr>
